f5115feac172911244fede487ac0dfa345249086
Nguyen Quang Huy <ngquanghuy.work@gmail.com>
2026-03-27 11:55:08 +0700

    fix: verify token by library

--- src/middlewares/authRequired.js
+++ src/middlewares/authRequired.js
@@ -15,24 +15,8 @@ async function authRequired(req, res, next) {
       return res.error(401, 'Unauthorized')
     }
 
-    const [encodedHeader, encodedPayload, clientSignature] =
-      accessToken?.split('.') ?? []
-    if (!encodedHeader || !encodedPayload || !clientSignature) {
-      return res.error(401, 'Invalid token')
-    }
+    jwt.verify(accessToken, secret)
 
-    const hmac = crypto.createHmac('sha256', secret)
-    hmac.update(`${encodedHeader}.${encodedPayload}`)
-
-    const signature = hmac.digest('base64url')
-    if (signature !== clientSignature) {
-      return res.error(401, 'Unauthorized')
-    }
-
-    const payload = JSON.parse(atob(encodedPayload))
-    if (!payload.exp || payload.exp <= Date.now() / 1000) {
-      return res.error(401, 'Token expired')
-    }
     const currentUser = await userModel.findOne(payload.sub)
     if (!currentUser) {
       return res.error(401, 'Unauthorized')
@@ -42,6 +26,9 @@ async function authRequired(req, res, next) {
     req.token = accessToken
     next()
   } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.error(401, 'Token expired')
+    }
     return res.error(401, 'Invalid token or Unauthorized')
   }
 }
